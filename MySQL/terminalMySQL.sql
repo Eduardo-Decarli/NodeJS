bfdc82ab6f91dc39f05de9a934dd15d2de239e52
Eduardo Decarli <decarliedu@gmail.com>
2024-09-04 11:49:05 -0300

    finalizando conteudo do modulo MySQL

--- MySQL/terminalMySQL.sql
+++ MySQL/terminalMySQL.sql
@@ -9,7 +9,7 @@ SHOW DATABASES; -- Comando mostra as tabelas do servidor logado
 CREATE DATABASE nomeDoBanco; -- Cria um novo banco de dados
 USE nomeDoBanco; -- Acessa o banco de dados especificado
 SHOW TABLES; -- Mostra as tabelas presentes no banco de dados atual
-CREATE TABLE usuarios() -- Cria a tabela usuarios, as colunas são especificadas dentro do parenteses
+CREATE TABLE usuarios(;) -- Cria a tabela usuarios, as colunas são especificadas dentro do parenteses
 /* EXEMPLO */
 CREATE TABLE usuarios(
     nome VARCHAR(50), -- Especifica a criação de uma coluna do tipo VARCHAR com maximo de 50 caracteres por célula
@@ -17,10 +17,12 @@ CREATE TABLE usuarios(
     idade INT --Cria uma coluna idade com valor inteiro
 );
 
+--------------------------------------------------------------------------
+
 DESCRIBE usuarios --Mostra os Detalhes da tabela especificada
 
 COMANDOS DE MANIPULAÇÃO DE DADOS
-INSERT INTO usuarios()--Usado para inserir dados na tabela, INSERT usado para inserir INTO usado para especificar qual tabela
+INSERT INTO usuarios();--Usado para inserir dados na tabela, INSERT usado para inserir INTO usado para especificar qual tabela
 /* EXEMPLO */
 INSERT INTO usuarios(
     nome, email, idade -- Especifica que quer inserir dados nas colunas nome, email e idade
@@ -28,18 +30,25 @@ INSERT INTO usuarios(
     "Victor Lima", -- Irá para o valor em Nome
     "emailTeste@teste.com", -- Irá para o valor em email
     8 -- Irá para o valor em idade
-)
+);
 
-SELECT --Usado para selecionar dados da tabela
-SELECT * -- Seleciona todos os dados da tabela
-FROM -- Usado para especificar qual tabela sofrerá uma ação
+--------------------------------------------------------------------------
+
+SELECT; --Usado para selecionar dados da tabela
+SELECT *; -- Seleciona todos os dados da tabela
+FROM; -- Usado para especificar qual tabela sofrerá uma ação
+WHERE -- Especifica uma busca por parametros Especificos
 
 /* EXEMPLO */
 
 SELECT * FROM usuarios -- Seleciona todos os dados da tabela usuarios
 
-WHERE -- Especifica uma busca por parametros Especificos
-
-/* EXEMPLO */
-
 SELECT * FROM usuarios WHERE idade = 8 --Seleciona todos as linhas da tabela usuario onde a coluna idade é igual a 8
+
+--------------------------------------------------------------------------
+DELETE --Utilizado para deletar dados de uma tabela
+DELETE FROM usuarios -- Deleta toda a tabela usuarios
+DELETE FROM usuarios WHERE idade = 8 -- Deleta todos os registros das linhas que tem valor 8 na coluna idade
+UPDATE -- Usado para atualizar registros na tabela
+UPDATE usuarios SET nome = "Nome de Teste" -- Altera todos os nomes da tabela usuarios para "Nome de Teste"
+UPDATE usuarios SET nome = "Nome de Teste" WHERE nome = "Augusto Oliveira" -- Altera o nome atual com valor Augusto Oliveira para "Nome de Teste"
