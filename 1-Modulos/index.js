05f028c5069ca093471e4ec4cc1a654192b62635
Eduardo Decarli <decarliedu@gmail.com>
2024-09-04 22:05:46 -0300

    Implementação melhor de Explicação

--- 1-Modulos/index.js
+++ 1-Modulos/index.js
@@ -1,5 +1,5 @@
-//importa outros arquivos para dentro desse arquivo
-let soma = require('./soma.js')
+// Importa o módulo 'soma.js' para que possamos usar a função definida nele
+let soma = require('./soma.js');
 
-console.log(soma(2, 4))
-
+// Usa a função 'soma' importada para calcular a soma de 2 e 4
+console.log(soma(2, 4));
--- 1-Modulos/soma.js
+++ 1-Modulos/soma.js
@@ -1,5 +1,6 @@
 let calculos = function(a, b) {
     return a + b
 }
-//Adiciona o módulo calculos para uma exportação
+
+// Adiciona o módulo 'calculos' para exportação, permitindo que ele seja importado em outros arquivos
 module.exports = calculos;
