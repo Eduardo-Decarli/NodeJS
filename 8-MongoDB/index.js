c390387f095b6c5d0210ae5f731e23d3026e9dd1
Eduardo Decarli <decarliedu@gmail.com>
2024-09-08 21:38:54 -0300

    Teste Rebase #1

---
+++ 8-MongoDB/index.js
@@ -0,0 +1,2 @@
+//O mongo DB é um banco de dados não relacional orientado a documentos.
+//A cada novo registro no banco de dados, é criado um arquivo em especifico e lido como JSON, por isso é muito utilizado para JS
